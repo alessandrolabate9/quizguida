9318363ea743d1058b8df80c8a5b7fcddea0683e
Alessandro Labate <60689326+alessandrolabate9@users.noreply.github.com>
2023-04-28 15:40:47 +0200

    Add files via upload

--- api/domande.php
+++ api/domande.php
@@ -4,11 +4,12 @@
 header("Content-type: application/json");
 include('config.php');
 
+$numero=$_GET['numero'];
 $num = 1;
 $sql = "
 select testo
 from domande
-where id_domanda=".$num;
-$records = selectDB($conn, $sql)
+where 1=1 and id_domanda=".$numero;
+$records = selectDB($conn, $sql);
 print(json_encode($records));
 ?>
